0cd530d442d4a8906a37c033fe52cd07363a328d
stephanoD <stefan.durla@yahoo.com>
2017-06-10 13:26:10 +0300

    Added restController and Mongo integration

---
+++ src/main/java/hello/GreetingController.java
@@ -0,0 +1,30 @@
+package hello;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+
+    @Autowired
+    private CustomerRepository repository;
+
+
+    @RequestMapping("/greeting")
+    public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
+        return new Greeting(counter.incrementAndGet(),
+                            String.format(template, name));
+    }
+
+    @RequestMapping("/findByFirstName")
+    public Customer findByFirstName(@RequestParam(value="name", defaultValue="Alice") String name) {
+        return repository.findByFirstName(name);
+    }
+}
