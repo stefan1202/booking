e8f2c337cfacbec6bf20592da05da3b36b35a9a6
stephanoD <stefan.durla@yahoo.com>
2017-06-28 01:31:32 +0300

    Added restController and Mongo integration

---
+++ web/src/main/java/ro/rural/booking/web/configuration/SpringMongoConfig.java
@@ -0,0 +1,27 @@
+package ro.rural.booking.web.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.config.AbstractMongoConfiguration;
+
+import com.mongodb.Mongo;
+import com.mongodb.MongoClient;
+/**
+ * Created by stefan.durla on 28/06/2017.
+ */
+
+
+@Configuration
+public class SpringMongoConfig extends AbstractMongoConfiguration {
+
+    @Override
+    public String getDatabaseName() {
+        return "booking";
+    }
+
+    @Override
+    @Bean
+    public Mongo mongo() throws Exception {
+        return new MongoClient("mongodb://booking:bookingPassword@mongodb.test-project-stefand.svc:27017");
+    }
+}
